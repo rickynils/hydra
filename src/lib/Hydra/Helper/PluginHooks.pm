4dcb623d78c14a20eff2de7fb30debb3025c5017
Shea Levy <shea@shealevy.com>
2016-03-12 11:03:05 -0500

    Add a plugin to interact with the github status API.
    Mutliple <githubstatus> sections are possible:
    * jobs: regexp for jobs to match
    * inputs: the input which corresponds to the github repo/rev whose
      status we want to report. Can be repeated
    * authorization: Verbatim contents of the Authorization header. See
      https://developer.github.com/v3/#authentication.

--- src/lib/Hydra/Helper/PluginHooks.pm
+++ src/lib/Hydra/Helper/PluginHooks.pm
@@ -7,6 +7,18 @@ our @ISA = qw(Exporter);
 our @EXPORT = qw(
     notifyBuildFinished);
 
+sub notifyBuildStarted {
+    my ($plugins, $build) = @_;
+    foreach my $plugin (@{$plugins}) {
+        eval {
+            $plugin->buildStarted($build);
+        };
+        if ($@) {
+            print STDERR "$plugin->buildStarted: $@\n";
+        }
+    }
+}
+
 sub notifyBuildFinished {
     my ($plugins, $build, $dependents) = @_;
     foreach my $plugin (@{$plugins}) {
